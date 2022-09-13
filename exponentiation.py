14b9bf279644e672c5fc241d934871672a17ddf0
Sergey <lagovsp@gmail.com>
2022-09-13 06:55:09 +0300

    side exponentiation added

---
+++ exponentiation.py
@@ -0,0 +1,17 @@
+def qpow(a, x: int):
+    if x < 0:
+        return 1 / qpow(a, -x)
+    if x == 0:
+        return 1
+    if x == 1:
+        return a
+    if x == 2:
+        return a * a
+    return a * qpow(a, x - 1) if x % 2 != 0 else qpow(a, int(x / 2)) ** 2
+
+
+if __name__ == '__main__':
+    base, power = int(input()), int(input())
+    print(f'{base} to the power of {power}')
+    print(f'We say {qpow(base, power)}')
+    print(f'Python {base ** power}')
