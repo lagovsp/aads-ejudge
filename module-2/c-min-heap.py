ec1cbae1c20b604715498642a5c5f02284316431
lagovsp <lagovsp@gmail.com>
2022-10-26 02:29:01 +0300

    structure fully encapsulated

--- module-2/c-min-heap.py
+++ module-2/c-min-heap.py
@@ -4,7 +4,11 @@ import math
 import re
 
 
-class Node:
+class Vertex:
+    def __init__(self, k=None, v=None):
+        self.key = k
+        self.val = v
+
     @staticmethod
     def parent(i: int) -> (bool, int):
         if i == 0:
@@ -16,154 +20,128 @@ class Node:
         left = 2 * i + 1
         return left, left + 1
 
-    def __init__(self, k=None, v=None):
-        self.key = k
-        self.val = v
-
-    def str(self, mh: 'MinHeap', i: int) -> str:
-        par_found, pi = Node.parent(i)
-        if not par_found:
-            return f'[{self.key} {self.val}]'
-        return f'[{self.key} {self.val} {mh.get_node(pi)[1].key}]'
-
 
 class MinHeap:
     def __init__(self):
         self.data = []
         self.index = dict()
 
-    def get_node(self, i: int) -> (bool, Node):
-        if i < len(self.data):
-            return True, self.data[i]
-        return False, None
-
     def add(self, k: int, v: str):
         if k in self.index:
-            print('error')
-            return
+            raise Exception('adding present element')
         i = len(self.data)
-        self.data.append(Node(k=k, v=v))
+        self.data.append(Vertex(k=k, v=v))
         self.index.update({k: i})
         self.__sift_up(i)
 
     def set(self, k: int, v: str):
         i = self.index.get(k)
         if i is None:
-            print('error')
-            return
+            raise Exception('setting absent element')
         self.data[i].val = v
 
     def delete(self, k: int):
         i = self.index.get(k)
         if i is None:
-            print('error')
-            return
-        self.__swap_nodes(i, len(self.data) - 1)
+            raise Exception('deleting absent element')
+        self.__swap_vertices(i, len(self.data) - 1)
         self.index.pop(k)
         self.data.pop()
         self.__heapify(i)
 
-    def search(self, k: int):
+    def search(self, k: int) -> (bool, int, str):
         i = self.index.get(k)
         if i is None:
-            print('0')
-            return
-        print(f'1 {i} {self.data[i].val}')
+            return False, None, None
+        return True, i, self.data[i].val
 
-    def min(self):
+    def min(self) -> (int, int, str):
         if not self.data:
-            print('error')
-            return
-        print(f'{self.data[0].key} 0 {self.data[0].val}')
+            raise Exception('empty, no minimum element')
+        return self.data[0].key, 0, self.data[0].val
 
-    def max(self):
+    def max(self) -> (int, int, str):
         if not self.data:
-            print('error')
-            return
+            raise Exception('empty, no maximum element')
         max_i = len(self.data) - 1
-        found_par, stop = Node.parent(max_i)
+        found_par, stop = Vertex.parent(max_i)
         if found_par:
             for i in range(max_i, stop, -1):
                 max_i = i if self.data[max_i].key < self.data[i].key else max_i
-        print(f'{self.data[max_i].key} {max_i} {self.data[max_i].val}')
+        return self.data[max_i].key, max_i, self.data[max_i].val
 
-    def extract(self):
+    def extract(self) -> (int, str):
         if not self.data:
-            print('error')
-            return
-        print(f'{self.data[0].key} {self.data[0].val}')
-        self.__swap_nodes(0, len(self.data) - 1)
+            raise Exception('empty, nothing to extract')
+        key, val = self.data[0].key, self.data[0].val
+        self.__swap_vertices(0, len(self.data) - 1)
         self.index.pop(self.data[len(self.data) - 1].key)
         self.data.pop()
         self.__sift_down(0)
+        return key, val
 
-    def print(self):
-        layer, i, stop = 1, 0, False
-        while not stop:
-            ins, li = [None] * layer, 0
-            if i + layer < len(self.data):
-                upper_bound = i + layer
-            else:
-                stop = True
-                upper_bound = len(self.data)
-                it = len(self.data) - i - 1
-                while it < layer:
-                    ins[it] = '_'
-                    it += 1
-            while i < upper_bound:
-                ins[li] = self.data[i].str(self, i)
-                li += 1
-                i += 1
-            layer *= 2
-            print(' '.join(ins))
-
-    def __swap_nodes(self, lhs: int, rhs: int):
+    def __swap_vertices(self, lhs: int, rhs: int):
         self.index.update({self.data[lhs].key: rhs})
         self.index.update({self.data[rhs].key: lhs})
         self.data[lhs], self.data[rhs] = self.data[rhs], self.data[lhs]
 
     def __sift_up(self, i: int):
         while i > 0:
-            pi = Node.parent(i)[1]
+            pi = Vertex.parent(i)[1]
             if self.data[pi].key < self.data[i].key:
                 return
-            self.__swap_nodes(i, pi)
+            self.__swap_vertices(i, pi)
             i = pi
 
     def __sift_down(self, i: int):
-        left, right = Node.children(i)
+        left, right = Vertex.children(i)
         while left < len(self.data):
             min_i = left
             if right < len(self.data) and self.data[right].key < self.data[left].key:
                 min_i = right
             if self.data[min_i].key > self.data[i].key:
                 return
-            self.__swap_nodes(i, min_i)
+            self.__swap_vertices(i, min_i)
             i = min_i
-            left, right = Node.children(i)
+            left, right = Vertex.children(i)
 
     def __heapify(self, i: int):
         if not self.data or not i < len(self.data):
             return
-        found_par, pi = Node.parent(i)
+        found_par, pi = Vertex.parent(i)
         if found_par and self.data[i].key < self.data[pi].key:
             self.__sift_up(i)
             return
         self.__sift_down(i)
 
 
+def print_heap(mh: MinHeap):
+    layer, i, stop = 1, 0, False
+    while not stop:
+        ins, li = [None] * layer, 0
+        if i + layer < len(mh.data):
+            upper_bound = i + layer
+        else:
+            stop, upper_bound, it = True, len(mh.data), len(mh.data) - i
+            while it < layer:
+                ins[it] = '_'
+                it += 1
+        while i < upper_bound:
+            v = mh.data[i]
+            if i == 0:
+                ins[li] = f'[{v.key} {v.val}]'
+            elif li == 0:
+                ins[li] = f'[{v.key} {v.val} {mh.data[v.parent(i)[1]].key}]'
+            else:
+                ins[li] = f'[{v.key} {v.val} {mh.data[v.parent(i)[1]].key}]'
+            li += 1
+            i += 1
+        layer *= 2
+        print(' '.join(ins))
+
+
 def main():
     mh = MinHeap()
-    methods = {
-        'add': MinHeap.add,
-        'set': MinHeap.set,
-        'delete': MinHeap.delete,
-        'search': MinHeap.search,
-        'min': MinHeap.min,
-        'max': MinHeap.max,
-        'extract': MinHeap.extract,
-        'print': MinHeap.print,
-    }
 
     while True:
         try:
@@ -172,16 +150,41 @@ def main():
             break
         if not line:
             continue
-        if re.match(r'^(min|max|print|extract)$', line):
-            methods[line](mh)
-            continue
-        if re.match(r'^(delete|search) (0|(-?[1-9]\d*))$', line):
-            c, k = re.split(' ', line)
-            methods[c](mh, int(k))
-            continue
-        if re.match(r'^(add|set) (0|(-?[1-9]\d*)) .*$', line):
-            c, k, v = re.split(' ', line)
-            methods[c](mh, int(k), v)
+        try:
+            if line == 'print':
+                print_heap(mh)
+                continue
+            if line == 'min':
+                k, i, v = mh.min()
+                print(f'{k} {i} {v}')
+                continue
+            if line == 'max':
+                k, i, v = mh.max()
+                print(f'{k} {i} {v}')
+                continue
+            if line == 'extract':
+                k, v = mh.extract()
+                print(f'{k} {v}')
+                continue
+            if re.fullmatch(r'delete (0|(-?[1-9]\d*))', line):
+                _, k = re.split(' ', line)
+                mh.delete(int(k))
+                continue
+            if re.fullmatch(r'search (0|(-?[1-9]\d*))', line):
+                _, k = re.split(' ', line)
+                status, i, v = mh.search(int(k))
+                print(f'1 {i} {v}' if status else '0')
+                continue
+            if re.fullmatch(r'add (0|(-?[1-9]\d*)) .*', line):
+                _, k, v = re.split(' ', line)
+                mh.add(int(k), v)
+                continue
+            if re.fullmatch(r'set (0|(-?[1-9]\d*)) .*', line):
+                _, k, v = re.split(' ', line)
+                mh.set(int(k), v)
+                continue
+        except Exception:
+            print('error')
             continue
         print('error')
 
