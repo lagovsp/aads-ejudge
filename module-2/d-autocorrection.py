4eb3aa9510363f7eb47e9c866cd880c778ae923c
lagovsp <lagovsp@gmail.com>
2022-11-07 03:44:00 +0300

    comments and proofs added

--- module-2/d-autocorrection.py
+++ module-2/d-autocorrection.py
@@ -1,7 +1,6 @@
 # Copyright Sergey Lagov 2022 lagovsp@gmail.com
 
 from enum import Enum
-import math
 
 
 class Trie:
@@ -11,110 +10,137 @@ class Trie:
         UNKNOWN = 2
 
     class Node:
-        def __init__(self, li=None, pl=None, p=None, t=None):
-            self.list_index = li  # Какой индекс у слова в списке слов дерева, префиксом которого это слово является
-            self.pref_len = pl  # Префиксом какой длины слово этой вершины является для листа
-            self.parent = p
-            self.children = dict()  # Ключи - первые символы следующих переходов, значения - вершины
+        def __init__(self, s=None, t=None):
+            self.substr = s
             self.is_terminal = t  # Заканчивается ли здесь слово
-
-        def split(self, split_len: int, words: list) -> 'Node':
-            if not 0 < split_len < self.parent.pref_len - self.pref_len:
-                raise Exception('split length error')
-            nn = Trie.Node(li=self.list_index, pl=split_len, p=self.parent, t=False)
-            self.parent.children[words[self.list_index][self.parent.pref_len + 1]] = nn
-            self.parent = nn
-            nn.children[words[self.list_index][nn.pref_len + 1]] = self
-            return nn
-
-    @staticmethod
-    def damerau_levenshtein_dist(w1: str, w2: str) -> int:
-        '''
-        Алгоритм поиска расстояния Дамерау-Левенштейна между двумя строками.
-        Заполняем таблицу, как продемонстрировано в конспекте (с. 142)
-
-        Временная сложность в худшем случае - O(|a|*|b|),
-        так как в матрице |a|*|b| клеток
-
-        Временная сложность в лучшем случае (хотя бы одна из строк пустая) - O(1)
-
-        Пространственная сложность - O(min(|a|,|b|)).
-        Выбираем наименьшее из слов, располагаем его горизонтально,
-        чтобы длина строки была минимальна.
-        Для подсчёта клетки необходимы лишь 3 последние строки - храним только их
-        '''
-
-        if not w1 or not w2:
-            return max(len(w1), len(w2))
-        if len(w2) < len(w1):
-            w1, w2 = w2, w1
-        lines = [  # prev_prev, prev, and last
-            [i for i in range(len(w1) + 1)],
-            [None] * (len(w1) + 1),
-            [None] * (len(w1) + 1),
-        ]
-        cur_line = 1
-        while not cur_line > len(w2):
-            i, pi, ppi = cur_line % 3, (cur_line - 1) % 3, (cur_line - 2) % 3
-            lines[i][0] = cur_line
-            for j in range(1, len(w1) + 1):
-                s = 0 if w1[j - 1] == w2[cur_line - 1] else 1
-                transposition = math.inf
-                if cur_line > 1 and j > 1:
-                    t = 1 if w1[j - 1] == w2[cur_line - 2] and w1[j - 2] == w2[cur_line - 1] else math.inf
-                    transposition = lines[ppi][j - 2] + t
-                lines[i][j] = min(
-                    lines[i][j - 1] + 1,
-                    lines[pi][j] + 1,
-                    lines[pi][j - 1] + s,
-                    transposition,
-                )
-            cur_line += 1
-        return lines[(cur_line - 1) % 3][-1]
+            self.children = dict()  # Ключи - первые символы следующих переходов, значения - вершины
+            # print(f'Trie.Node created {self}')
+            # print(f'    self.substr = "{self.substr}"')
+            # print(f'    self.is_terminal = "{self.is_terminal}"')
+            # print(f'    self.children = "{self.children}"')
 
     def __init__(self):
-        self.root = Trie.Node()
-        # Здесь хранятся терминальные строки.
-        # Вершины содержат ссылки на слова из списка и длины префикса, которым эти вершины соответствуют
-        # Строки в Python очень тяжелые. Если бы мы хранили строки в вершинах, было бы n строк.
-        # При таком подходе количество строк равно количеству листьев, два int должны весить меньше
-        self.words = list()
+        self.root = Trie.Node(s='', t=False)
+
+    @staticmethod
+    def __split(p: 'Trie.Node', child: 'Trie.Node', cpl: int, word: str):
+        med = Trie.Node(s=child.substr[:cpl], t=False)
+        child.substr = child.substr[cpl:]
+        child.parent = med
+        new = Trie.Node(s=word, t=True)
+        med.children = {child.substr[0]: child, word[0]: new}
+        p.children.update({med.substr[0]: med})
+
+    @staticmethod
+    def __mark_terminal(p: 'Trie.Node', child: 'Trie.Node', i: int):
+        new = Trie.Node(s=child.substr[:i], p=p, t=True)
+        new.children.update({child.substr[i + 1]: child})
+        p.children[child.substr[0]] = new
+        child.parent = new
 
     def add_word(self, word: str):
-        cur_index, cur_node = 0, self.root
-        while True:
-            child = cur_node.children.get(word[cur_index])
-            if child is None:
-                self.words.append(word)
-                cur_node.children.update({word[cur_index]: Trie.Node(
-                    len(self.words) - 1, len(word), p=cur_node, t=True)})
-                return
-            i = 0
-            index = child.list_index
-            # for i in range(child.parent.pref_len + 1, child.parent.pref_len + child.pref_len):
-            #     if
-            # while self.words[index]
-            # if self.words[child.list_index][:child.pref_len]
-            #     if len(child)
-            #         cur_index += 1
+        '''
+        Алгоритм добавления слова в сжатое префиксное дерево (далее - словарь).
+        Совершаем переходы только по нужным нам буквам, параллельно сверяя текущий путь с добавляемым словом word.
 
-    def check_word(self, word: str) -> (ResultType, list):
-        return Trie.ResultType.MATCHED, []
+        Временная сложность алгоритма - O(k), где k - длина добавляемого слова word.
+        Пространственная сложность алгоритма - O(1).
+        '''
+
+        pass
+
+    def find_word(self, word: str) -> bool:
+        '''
+        Алгоритм поиска слова (word) в словарь.
+
+        Переходим только в вершины, первая буква перехода (добавляемой подстроки) которых соответствует следующей букве
+        искомого слова. Остальные вершины проверять нет смысла. Если слово закончилось и мы находимся в терминальной
+        вершине (на ней заканчивается слово в словаре), то возвращаем True. Иначе - False.
+
+        Временная сложность алгоритма - O(k), где k - длина запроса (слово word).
+        Пространственная сложность алгоритма - O(1).
+        '''
+
+        if not word and self.root.is_terminal:
+            return True
+        cur_node, cur_str, word_i, cur_str_i = self.root, self.root.substr, 0, 0
+        while word_i < len(word) and cur_node is not None:
+            if word[word_i] != cur_str[cur_str_i]:
+                return False
+            if cur_str_i != len(cur_str) - 1:
+                continue
+            if word_i == len(word) - 1 and cur_node.is_terminal:
+                return True
+            if not word_i + 1 < len(word):
+                return False
+            cur_node, cur_str_i = cur_node.children.get(word[word_i + 1]), 0
+            word_i += 1
+        return False
+
+    def suggest(self, word: str) -> (ResultType, list):
+        '''
+        Алгоритм поиска слов в словаре, чьи расстояния Дамерау-Левенштейна (далее - Д-Л) до запроса word не больше 1.
+        Рекурсивный алгоритм обходит словарь в глубину, накапливая расстояние Д-Л, до момента, пока оно не больше 1.
+
+        Временная сложность алгоритма - O(m * n^2), где m - мощность алфавита, а n - длина запроса.
+
+        Докажем эту оценку.
+
+        Рассмотрим худший случай. Сжатое префиксное дерево выродилось в полное m-арное префиксное дерево,
+        в каждой вершине которого находится лишь 1 символ.
+
+        Пусть слово word при проверке дойдет до листа (и будет отстоять по Д-Л на 1 от находящегося в словаре
+        корректного слова, т.е. ошибка в слове word, например, - лишний символ на конце).
+
+        Пусть i - счетчик пройденных ярусов дерева. Идем от корня, i = 0.
+
+        При каждом переходе на следующий ярус мы должны будем проверить как путь, содержащий в себе слово word,
+        так и остальные m-1 вершин, так как счетчик ошибок равен нулю (при переходе в каждую из m-1 вершин счетчик
+        увеличивается на 1).
+
+        Так как мы рассматриваем случай, где поиск дойдет до листов дерева, а длина запроса - n, то высота дерева,
+        которая ранее определялась как log<m>(|V|) (логарифм по основанию m от мощности мн-ва V), где V - мн-во
+        вершин дерева, теперь будет определяться как n => h = n.
+
+        Типы ошибок по Д-Л: вставка (В), замена (З), удаление (У), транспозиция (Т).
+
+        В случае, если при переходе в какую-то из m-1 вершин мы увидели одну из ошибок (В), (З) или (У), то у текущего
+        пути более нет права на ошибку => существует максимум 1 путь до листа, причем его длина будет h-i для i-ого
+        яруса сверху.
+
+        Если мы посчитали, что найдена ошибка (Т), то буква следующего перехода определена - та, которая поменяется
+        местами с предыдущей (т.е. существует только 1 возможный вариант). Таким образом, для ошибки (Т) у нас также
+        существует максимум 1 путь до листа, и его длина также равна h-i для i-ого яруса сверху.
+
+        Запишем следующую сумму (все далее записанные суммы идут от i = 0 до n):
+
+        ∑(1+(m-1)(n-i)) = n+∑(mn-n-im+i) = n+mn^2-n^2-m∑i+∑i = n^2(m-1)+n+(1-m)∑i = n^2(m-1)+n+(1-m)((0+n)/2*n) =
+        = n^2(m-1)+n+(1-m)(n^2/2) = n^2(m-1)+n+n^2/2-mn^2/2 = n^2((m-1)+1/2-m/2)+n = n^2(m-1)/2+n = O(mn^2)
+
+        Временная сложность алгоритма доказана.
+
+        Пространственная сложность рекурсивного алгоритма - O(n), так как в стеке вызова может быть n кадров функции.
+        '''
+
+        pass
 
 
 def main():
     # w1 = 'список'
     # w2 = 'кипяток'
 
-    w1 = 'вестибулярный'
-    w2 = 'весна'
-
-    print(Trie.damerau_levenshtein_dist(w1, w2))
+    # w1 = 'вестибулярный'
+    # w2 = 'весна'
 
     t, length = Trie(), int(input())
 
-    for i in range(length):
-        t.add_word(input())
+    i = 0
+    while i < length:
+        word = input()
+        if not word:
+            continue
+        t.add_word(word)
+        i += 1
 
     while True:
         try:
@@ -123,8 +149,8 @@ def main():
             break
         if not line:
             continue
-        status, suggests = t.check_word(line)
 
+        status, suggests = t.suggest(line)
         if status == Trie.ResultType.MATCHED:
             print(f'{line} - ok')
             continue
