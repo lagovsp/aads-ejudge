4138f08ce230195a8d08cae07c41320f7199865f
lagovsp <lagovsp@gmail.com>
2022-11-10 02:41:44 +0300

    removed deprecated solution

--- module-1/a-sum.py
+++ module-1/a-sum.py
@@ -4,20 +4,24 @@ from string import digits
 import sys
 
 
-def get_sum() -> int:
-    nums = []
+def get_sum() -> int:  # w/o excess data structures
+    s = 0
     minus, prev_digit = False, False
     for line in sys.stdin:
+        current = 0
         for c in line:
             if c in digits:
                 if prev_digit:
-                    nums[-1] = nums[-1] * 10 + (-int(c) if minus else int(c))
+                    current = current * 10 + (-int(c) if minus else int(c))
                 else:
-                    nums.append(-int(c) if minus else int(c))
+                    current = -int(c) if minus else int(c)
                 prev_digit = True
             else:
+                s += current
+                current = 0
                 minus, prev_digit = True if c == '-' else False, False
-    return sum(nums)
+        s += current
+    return s
 
 
 if __name__ == '__main__':
