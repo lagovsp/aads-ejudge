e30f93887a69b07fdb34cd3060a7de55d32416e5
lagovsp <lagovsp@gmail.com>
2022-11-13 03:06:15 +0300

    m1-a tests added, README fixed, script adjusted

--- scripts/check.sh
+++ scripts/check.sh
@@ -9,20 +9,20 @@ output="output.txt"
 
 for ((i = 1; i <= $3; ++i)); do
   printf "Test %2s:" $i
-  if ! [ -f "$2"/input/input"$i".txt ]; then
-    echo "                       File $2/input/input$i.txt not found"
+  if ! [ -f "$2"/dat/"$i".dat ]; then
+    echo "                       File $2/dat/$i.dat not found"
     continue
   fi
 
   start=$(($(date +%s%N) / 1000000))
-  T=$({ /usr/bin/time -f "%MKB" python3 "$1" >"$output" <"$2"/input/input"$i".txt; } 2>&1)
+  T=$({ /usr/bin/time -f "%MKB" python3 "$1" >"$output" <"$2"/dat/"$i".dat; } 2>&1)
   end=$(($(date +%s%N) / 1000000))
   runtime=$((end - start)) # milliseconds
   secs=$((runtime / 1000))
   milliseconds=$((runtime - secs * 1000))
   printf "%3s." $secs
   printf "%03ds %13s " $milliseconds $T
-  python3 checker.py "$2"/answers/answer"$i".txt output.txt
+  python3 checker.py "$2"/ans/"$i".ans output.txt
 done
 
 rm "$output"
